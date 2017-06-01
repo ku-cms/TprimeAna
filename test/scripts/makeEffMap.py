17b292db8ccff19c25069df4529996a5c555f36d
Erich <erichjs@hotmail.com>
2017-06-01 15:49:51 -0500

    change pt and eta binning

--- test/scripts/makeEffMap.py
+++ test/scripts/makeEffMap.py
@@ -25,15 +25,21 @@ def subjetEff(inFile, jetType, fileOut):
     t.SetBranchAddress('hadronFlavourSJ0AK8', sj0Flavour)
     t.SetBranchAddress('hadronFlavourSJ1AK8', sj1Flavour) 
 
-    h_num_b = rt.TH2D("num_b", "Numerator for b in "+inFile.split('/')[-1].split('.')[0],50,200,1000,24,0,2.4)
-    h_num_c = rt.TH2D("num_c", "Numerator for c in "+inFile.split('/')[-1].split('.')[0],50,200,1000,24,0,2.4)
-    h_num_l = rt.TH2D("num_l", "Numerator for l in "+inFile.split('/')[-1].split('.')[0],50,200,1000,24,0,2.4)
-    h_denom_b = rt.TH2D("denom_b", "Denominator for b in "+inFile.split('/')[-1].split('.')[0],50,200,1000,24,0,2.4)
-    h_denom_c = rt.TH2D("denom_c", "Denominator for c in "+inFile.split('/')[-1].split('.')[0],50,200,1000,24,0,2.4)
-    h_denom_l = rt.TH2D("denom_l", "Denominator for l in "+inFile.split('/')[-1].split('.')[0],50,200,1000,24,0,2.4)
-    h_eff_b = rt.TH2D("eff_b", "Efficiency for b in "+inFile.split('/')[-1].split('.')[0],50,200,1000,24,0,2.4)
-    h_eff_c = rt.TH2D("eff_c", "Efficiency for c in "+inFile.split('/')[-1].split('.')[0],50,200,1000,24,0,2.4)
-    h_eff_l = rt.TH2D("eff_l", "Efficiency for l in "+inFile.split('/')[-1].split('.')[0],50,200,1000,24,0,2.4)
+    ptBins = array.array('f', [0.,20.,30.,120.,180.,240.,450.,1000.])
+    etaBins = array.array('f', [0.,2.4])
+
+    nptBins = len(ptBins) - 1
+    netaBins = len(etaBins) - 1
+
+    h_num_b = rt.TH2D("num_b", "Numerator for b in "+inFile.split('/')[-1].split('.')[0],nptBins,ptBins,netaBins,etaBins)
+    h_num_c = rt.TH2D("num_c", "Numerator for c in "+inFile.split('/')[-1].split('.')[0],nptBins,ptBins,netaBins,etaBins)
+    h_num_l = rt.TH2D("num_l", "Numerator for l in "+inFile.split('/')[-1].split('.')[0],nptBins,ptBins,netaBins,etaBins)
+    h_denom_b = rt.TH2D("denom_b", "Denominator for b in "+inFile.split('/')[-1].split('.')[0],nptBins,ptBins,netaBins,etaBins)
+    h_denom_c = rt.TH2D("denom_c", "Denominator for c in "+inFile.split('/')[-1].split('.')[0],nptBins,ptBins,netaBins,etaBins)
+    h_denom_l = rt.TH2D("denom_l", "Denominator for l in "+inFile.split('/')[-1].split('.')[0],nptBins,ptBins,netaBins,etaBins)
+    h_eff_b = rt.TH2D("eff_b", "Efficiency for b in "+inFile.split('/')[-1].split('.')[0],nptBins,ptBins,netaBins,etaBins)
+    h_eff_c = rt.TH2D("eff_c", "Efficiency for c in "+inFile.split('/')[-1].split('.')[0],nptBins,ptBins,netaBins,etaBins)
+    h_eff_l = rt.TH2D("eff_l", "Efficiency for l in "+inFile.split('/')[-1].split('.')[0],nptBins,ptBins,netaBins,etaBins)
 
 
 
@@ -86,4 +92,27 @@ def subjetEff(inFile, jetType, fileOut):
     fout.Close()
 
 if __name__ == "__main__":
+    subjetEff('./TbtH_800_LH.root', 'CHS', './TbtH_800_LH_btagEff_loose.root')
+    subjetEff('./TbtH_900_LH.root', 'CHS', './TbtH_900_LH_btagEff_loose.root')
+    subjetEff('./TbtH_1000_LH.root', 'CHS', './TbtH_1000_LH_btagEff_loose.root')
     subjetEff('./TbtH_1200_LH.root', 'CHS', './TbtH_1200_LH_btagEff_loose.root')
+    subjetEff('./TbtH_1500_LH.root', 'CHS', './TbtH_1500_LH_btagEff_loose.root')
+    subjetEff('./TbtH_1800_LH.root', 'CHS', './TbtH_1800_LH_btagEff_loose.root')
+    subjetEff('./TbtH_800_RH.root', 'CHS', './TbtH_800_RH_btagEff_loose.root')
+    subjetEff('./TbtH_900_RH.root', 'CHS', './TbtH_900_RH_btagEff_loose.root')
+    subjetEff('./TbtH_1000_RH.root', 'CHS', './TbtH_1000_RH_btagEff_loose.root')
+    subjetEff('./TbtH_1200_RH.root', 'CHS', './TbtH_1200_RH_btagEff_loose.root')
+    subjetEff('./TbtH_1500_RH.root', 'CHS', './TbtH_1500_RH_btagEff_loose.root')
+    subjetEff('./TbtH_1800_RH.root', 'CHS', './TbtH_1800_RH_btagEff_loose.root')
+    subjetEff('./TttH_800_LH.root', 'CHS', './TttH_800_LH_btagEff_loose.root')
+    subjetEff('./TttH_900_LH.root', 'CHS', './TttH_900_LH_btagEff_loose.root')
+    subjetEff('./TttH_1000_LH.root', 'CHS', './TttH_1000_LH_btagEff_loose.root')
+    subjetEff('./TttH_1200_LH.root', 'CHS', './TttH_1200_LH_btagEff_loose.root')
+    subjetEff('./TttH_1500_LH.root', 'CHS', './TttH_1500_LH_btagEff_loose.root')
+    subjetEff('./TttH_1800_LH.root', 'CHS', './TttH_1800_LH_btagEff_loose.root')
+    subjetEff('./TttH_800_RH.root', 'CHS', './TttH_800_RH_btagEff_loose.root')
+    subjetEff('./TttH_900_RH.root', 'CHS', './TttH_900_RH_btagEff_loose.root')
+    subjetEff('./TttH_1000_RH.root', 'CHS', './TttH_1000_RH_btagEff_loose.root')
+    subjetEff('./TttH_1200_RH.root', 'CHS', './TttH_1200_RH_btagEff_loose.root')
+    subjetEff('./TttH_1500_RH.root', 'CHS', './TttH_1500_RH_btagEff_loose.root')
+    subjetEff('./TttH_1800_RH.root', 'CHS', './TttH_1800_RH_btagEff_loose.root')
